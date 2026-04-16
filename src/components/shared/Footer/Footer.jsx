e8c98e2af12eefe1c2d705fe5c79faa15ee6086c
anikh174 <hossainanik174@gmail.com>
2026-04-16 15:33:34 +0600

    added footer section with responsive

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -5,7 +5,28 @@ import { FaXTwitter } from 'react-icons/fa6';
 const Footer = () => {
     return (
         <div className='bg-[#244D3F]'>
-            
+            <div className='p-10 container mx-auto'>
+                <div className='space-y-4 text-white text-center'>
+                    <h1 className='text-5xl font-extrabold'>KeenKeeper</h1>
+
+                    <p>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+                    <p>Social Links</p>
+                    <div className='flex gap-3 justify-center'>
+                        <p><FaInstagram /></p>
+                        <p><FaFacebook /></p>
+                        <p><FaXTwitter /></p>
+                    </div>
+                </div>
+
+                <div className='flex md:flex-row flex-col justify-between text-white items-center mt-8 gap-3'>
+                    <p>© 2026 KeenKeeper. All rights reserved.</p>
+                    <div className='flex gap-3'>
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Cookies</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
