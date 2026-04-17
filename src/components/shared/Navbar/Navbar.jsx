159b9fe100ce7ebe726d25fc7371118f7c6981a3
anikh174 <hossainanik174@gmail.com>
2026-04-17 20:31:05 +0600

    fix some issue

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -28,12 +28,12 @@ const Navbar = () => {
   return (
     <div className=" bg-base-100 shadow-sm px-2 md:px-10 lg:px-0">
       <div className="flex justify-between items-center container mx-auto py-4">
-        <h1 className="text-[#244D3F] text-2xl font-bold"><span className="text-black font-bold md:font-extrabold">Keen</span>Keeper</h1>
+        <h1 className="text-[#244D3F] text-2xl md:text-4xl font-bold"><span className="text-black font-bold md:font-extrabold">Keen</span>Keeper</h1>
 
         <div>
             <ul className="flex gap-1 md:gap-3 items-center">
                 {navItems.map((item, ind)=>
-                  <MyLink key={ind} href={item.path}><div className="flex gap-1 items-center">{item.icon}{item.text}</div></MyLink>
+                  <MyLink key={ind} href={item.path}><div className="flex gap-1 items-center text-[10px] md:text-lg">{item.icon}{item.text}</div></MyLink>
                 )}
             </ul>
         </div>
